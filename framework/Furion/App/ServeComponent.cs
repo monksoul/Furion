4b67d6b05efd65d0c057957912da9cb3e213d678
MonkSoul <monksoul@outlook.com>
2026-07-08 07:46:01 +0800

    😊 修复 `Serve.Run()` 在 `File-Based Apps` 应用中出现异常

--- framework/Furion/App/ServeComponent.cs
+++ framework/Furion/App/ServeComponent.cs
@@ -28,6 +28,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Text.Encodings.Web;
+using System.Text.Json.Serialization.Metadata;
 
 namespace Furion.Components;
 
@@ -55,6 +56,7 @@ public sealed class ServeServiceComponent : IServiceComponent
                 .AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+                    options.JsonSerializerOptions.TypeInfoResolver = new DefaultJsonTypeInfoResolver();
                 })
                 .AddInjectWithUnifyResult();
     }
