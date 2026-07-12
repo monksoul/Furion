288489785b5b9eb55738220b99978bf241afd906
MonkSoul <monksoul@outlook.com>
2026-07-12 14:19:04 +0800

    😊 发布 `v4.9.9.24` 版本，改进 `HTTP` 远程请求底层

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.9.23";
+$FurTools = "Furion Tools v4.9.9.24";
 
 # 输出信息
 $copyright = @"
