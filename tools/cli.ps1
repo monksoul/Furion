e0c4e200d49172e80f07d1eadfe466a8f9b053f4
MonkSoul <monksoul@outlook.com>
2026-07-16 08:56:00 +0800

    😊 发布 `v4.9.9.32` 版本，改进 `JWT` 授权性能

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.9.31";
+$FurTools = "Furion Tools v4.9.9.32";
 
 # 输出信息
 $copyright = @"
