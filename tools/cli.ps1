9fb5fd8d1b1800e59fcbac7857722085b5313bdb
MonkSoul <monksoul@outlook.com>
2026-07-16 10:59:01 +0800

    😊 发布 `v4.9.9.33` 版本，修正定时任务看板登录页面密码框明文问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.9.32";
+$FurTools = "Furion Tools v4.9.9.33";
 
 # 输出信息
 $copyright = @"
