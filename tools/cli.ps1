0e27a1c9f7d366c84b9a9e73d95ee0597207af2a
MonkSoul <monksoul@outlook.com>
2026-08-02 00:25:21 +0800

    😊 发布 `v4.9.9.53` 版本，修复 `File-Based Apps` 项目类型支持

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.9.52";
+$FurTools = "Furion Tools v4.9.9.53";
 
 # 输出信息
 $copyright = @"
