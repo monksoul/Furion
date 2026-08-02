4396dc7a1ca2e28247a946e23551c92de57e49a9
MonkSoul <monksoul@outlook.com>
2026-08-02 23:18:28 +0800

    😊 发布 `v4.9.9.57` 版本，改进 `HTTP` 远程请求下载文件时文件名的解析逻辑

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.9.56";
+$FurTools = "Furion Tools v4.9.9.57";
 
 # 输出信息
 $copyright = @"
