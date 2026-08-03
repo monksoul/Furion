5df67d40b515ef45f7857f86368a3aa00185148a
MonkSoul <monksoul@outlook.com>
2026-08-04 03:23:17 +0800

    😊 发布 `v4.9.9.60` 版本，改进日志模块解析堆栈信息性能（提升 70% 以上）

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.9.59";
+$FurTools = "Furion Tools v4.9.9.60";
 
 # 输出信息
 $copyright = @"
