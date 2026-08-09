e389245eca29972d86ad544e4d22710b7e74021a
MonkSoul <monksoul@outlook.com>
2026-08-09 16:41:26 +0800

    😊 发布 `v4.9.9.68` 版本，新增 `HTTP` 远程请求发送文件内容和二进制流内容

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.9.67";
+$FurTools = "Furion Tools v4.9.9.68";
 
 # 输出信息
 $copyright = @"
