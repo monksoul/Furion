3ae287b0e7e3b3111682047d5214877eb46a7d35
MonkSoul <monksoul@outlook.com>
2026-08-17 12:05:23 +0800

    😊 发布 `v4.9.9.80` 版本，修复 `JSON` 不支持科学计数法数值转 `long` 类型问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.9.79";
+$FurTools = "Furion Tools v4.9.9.80";
 
 # 输出信息
 $copyright = @"
