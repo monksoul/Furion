7264cd9bd697ba06a790f8fa5fda3a868ed9af2c
MonkSoul <monksoul@outlook.com>
2026-08-17 16:29:19 +0800

    😊 发布 `v4.9.9.81` 版本，改进 `HTTP` 远程请求超时和重试的语义问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.9.80";
+$FurTools = "Furion Tools v4.9.9.81";
 
 # 输出信息
 $copyright = @"
