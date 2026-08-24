62d76ef4242eec4dcaf4a410cd1c32f78c12883e
MonkSoul <monksoul@outlook.com>
2026-08-25 02:34:44 +0800

    😊 发布 `v4.9.9.84` 版本，改进 `JSON` 序列化转换器（枚举/时间等类型）

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.9.83";
+$FurTools = "Furion Tools v4.9.9.84";
 
 # 输出信息
 $copyright = @"
