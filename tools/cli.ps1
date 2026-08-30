61ba839882215e4ee512355f075fee681743ea07
MonkSoul <monksoul@outlook.com>
2026-08-30 12:58:15 +0800

    😊 发布 `v4.9.9.91` 版本，移除日志模块字符串拓展方法 ⚠️

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -194,7 +194,7 @@ function NormalizeIndent($code, $indent) {
     return $normalizedLines -join [Environment]::NewLine
 }
 
-$FurTools = "Furion Tools v4.9.9.90"
+$FurTools = "Furion Tools v4.9.9.91"
 
 # 输出信息
 $copyright = @"
